fc0717639c7865599962dadd121452b34b696b9d
akbar.dzhuraev <akbar.dzhuraev@westwing.ru>
2021-07-25 22:23:05 +0200

    Initialize project

---
+++ app/app/Http/Controllers/Controller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        return new JsonResponse(['status' => 'ok']);
+    }
+}
